9d7f7a0cbd31842b4993c6af5d48a29712f3755c
lanme2019 <809538476@qq.com>
2020-10-14 15:18:42 +0800

    20200821

---
+++ rabbitmq/src/main/java/com/lanme/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,26 @@
+package com.lanme.rabbitmq.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author lanme2019
+ * @version 1.0
+ * @date 2020/8/21 11:26
+ */
+
+@Configuration
+public class RabbitMQConfig {
+
+    public void a() throws Exception{
+        b();
+    }
+
+    public void b() throws {
+        try {
+
+        }catch (Exception e){
+            throw e;
+        }
+    }
+}
